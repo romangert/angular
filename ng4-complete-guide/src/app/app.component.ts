caa2bed1b8b6ea24752fd5ccc725f0fc465e66d7
Roman Gr <roman.gert@gmail.com>
2021-04-28 17:58:09 +0300

    services

--- ng4-complete-guide/src/app/app.component.ts
+++ ng4-complete-guide/src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account.service';
 
 import { HeaderComponent } from "./header/header.component";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AccountService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   loadedFeature: string = 'recipe';
+  accounts: { name: string; status: string; }[]=[];
+
   onNavigate(feature: string){
     this.loadedFeature=feature;
   }
+
+  constructor(private accountService: AccountService){}
+  ngOnInit(): void {
+    this.accounts=this.accountService.accounts;
+  }
+  
 }
