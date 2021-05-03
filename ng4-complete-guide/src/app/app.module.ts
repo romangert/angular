b38269f5c5eedb45463d74005632888f586080bb
Roman Gr <roman_gr@malam.com>
2021-05-03 11:09:48 +0300

    fix conflicts

--- ng4-complete-guide/src/app/app.module.ts
+++ ng4-complete-guide/src/app/app.module.ts
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 import { UserService } from './users.service';
-=======
 import { ShopingListService } from './shopping-list/shopping-list.service';
->>>>>>> 749b12f5e35510c5bc2ec944074289c265a5e9b0
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from "./header/header.component";
 import { RecipesComponent } from './recipes/recipes.component';
@@ -41,11 +37,7 @@ import { InactiveUsersComponent } from './inactive-users/inactive-users.componen
   imports: [
     BrowserModule
   ],
-<<<<<<< HEAD
-  providers: [AccountService, LoggingService, UserService],
-=======
-  providers: [ShopingListService, AccountService, LoggingService],
->>>>>>> 749b12f5e35510c5bc2ec944074289c265a5e9b0
+  providers: [ShopingListService, AccountService, LoggingService, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
